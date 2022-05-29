c563a696c3db21e0365229243749c8963b0b1840
Mackenzie Pearce <102491128+MacPearce@users.noreply.github.com>
2022-05-29 23:51:43 +0000

    got bg working

--- main.py
+++ main.py
@@ -9,15 +9,14 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
-    screen = pygame.display.set_mode((780, 400))
     pygame.display.set_caption("Mac's Alien Invasion")
-    bg_color = (128, 0, 128)
+
     #ship = Ship(screen)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        screen.fill(bg_color)
+
         screen.fill(ai_settings.bg_color)
         #ship.blitme()
         pygame.display.flip()
