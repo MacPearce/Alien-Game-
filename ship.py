e92806c752a81687612dd36cc6bd9a94e0c6702d
Mackenzie Pearce <102491128+MacPearce@users.noreply.github.com>
2022-05-30 23:59:23 +0000

    ship working

--- ship.py
+++ ship.py
@@ -1,8 +1,7 @@
 import pygame
 
 class Ship():
-  
-   def __init__(self, screen):
+  def __init__(self, screen):
         self.screen = screen
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
@@ -11,6 +10,5 @@ class Ship():
         self.rect.bottom = self.screen_rect.bottom
 
 
-def blitme(self):
- """Draw the ship at its current location."""
- self.screen.blit(self.image, self.rect)
+  def blitme(self):
+    self.screen.blit(self.image, self.rect)
