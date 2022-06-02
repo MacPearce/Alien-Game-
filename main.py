fcaa4f59169eb364de4b2523613fb6eece4a0b0a
Mackenzie Pearce <102491128+MacPearce@users.noreply.github.com>
2022-06-02 04:59:09 +0000

    gf

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from ship import Ship
 from settings import Settings
+import game_functions as gf
 
 
 def run_game():
@@ -12,9 +13,14 @@ def run_game():
     pygame.display.set_caption("Mac's Alien Invasion")
     ship = Ship(screen)
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+      gf.check_events()
+      gf.update_screen(ai_settings, screen, ship)
+
+
+      
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          sys.exit()
         screen.fill(ai_settings.bg_color)
         ship.blitme()
         pygame.display.flip()
