087eafc3fe00f8bda287ddb82ceb5e6b10d4df5b
Mackenzie Pearce <102491128+MacPearce@users.noreply.github.com>
2022-06-05 23:40:43 +0000

    gf debugging

--- game funtion.py
+++ game funtion.py
@@ -1,7 +1,6 @@
 import sys  
 import pygame 
-def check_events(): 
-  """Respond to keypresses and mouse events."""
+def check_events():
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       sys.exit()
